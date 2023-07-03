25d256ed580b996b515adc1867f18def96664118
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-07-03 16:13:59 +0300

    delete basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/dashboard-ova-2">
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
