7bf5ce4121f90f1ce4d7fd52ce5c79794b184b0a
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-07-03 15:23:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dashboard-ova-2">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
