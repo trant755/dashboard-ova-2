d3ce8c5df1f205ebe1fb5a3c3e438ef7b44b4bf9
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-07-04 10:53:44 +0300

    fix sidebar menu

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -7,20 +7,20 @@ import * as SC from "./SideBar.styled";
 export default function MiniDrawer({ open, setOpen }) {
   const { page, sub, group } = useParams();
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  // const handleDrawerOpen = () => {
+  //   setOpen(true);
+  // };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  // const handleDrawerClose = () => {
+  //   setOpen(false);
+  // };
 
   return (
     <SC.Drawer
       variant="permanent"
       open={open}
-      onMouseEnter={handleDrawerOpen}
-      onMouseLeave={handleDrawerClose}
+      // onMouseEnter={handleDrawerOpen}
+      // onMouseLeave={handleDrawerClose}
       sx={{ paddingTop: { sm: "86px", lg: "0px" } }}
     >
       <NavList open={open} setOpen={setOpen} navRoutes={{ page, sub, group }} />
